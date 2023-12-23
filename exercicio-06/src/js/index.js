ae434874aadcfb0b36b2b1409a569e8aafdfbdf1
Evandro Santos <vandersant7@gmail.com>
2023-12-23 20:46:23 -0300

    para visual concluida

---
+++ exercicio-06/src/js/index.js
@@ -0,0 +1,9 @@
+const mostrarFaq = document.querySelectorAll('.content');
+
+const botaoFaq = document.querySelectorAll('.pergunta');
+
+
+botaoFaq[indice].addEventListener ('click', ()=>{
+    mostrarFaq[indice].classList.contains('selecionado');
+
+})
